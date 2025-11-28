632533ee00623faed7a5063a223979ab4d7377b0
raj45999 <rajbalyan459@gmail.com>
2025-11-28 18:21:48 +0530

    Assignment 1 Complete

---
+++ Assignment_1.py
@@ -0,0 +1,22 @@
+print("TASK_1:Please Enter two numbers for algebric calculations;-")
+
+x=int(input("Enter your first number:- "))
+y=int(input("Enter your second number:- "))
+
+a=x+y
+b=x-y
+c=x*y
+d=x/y
+
+print("Addition; ",a)
+print("Subtraction; ",b)
+print("Multiplication; ",c)
+print("Division; ",d)
+
+
+print("end of TASK 1")
+
+x2=input("Enter your first name;- ")
+y2=input("Enter your last name;- ")+"!"
+
+print("Hello,",x2,y2,"WElcome to the Python program",)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TudeDude
+Submission of Assignment 1
 Created by Raj for TudeDude projects
+
